abd4a2f66126efa929e8c646ce4855784edc062c
Lysir <60637411+Lysir-111@users.noreply.github.com>
2025-03-31 23:05:57 +0800

    Update index.tsx

--- app/_contexts/index.tsx
+++ app/_contexts/index.tsx
@@ -1,28 +1,41 @@
-"use client";
+'use client'
 
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react'
+import dynamic from 'next/dynamic'
+import { ColorModeProvider } from './color-mode'
 
-import { PrivyProvider } from "./privy";
-import { ColorModeProvider } from "./color-mode";
-import { PostHogProvider } from "./posthog";
+//  修改开始：动态加载可能引起 SSR 问题的提供者
+const PostHogProvider = dynamic(
+  () => import('./posthog').then(mod => mod.PostHogProvider),
+  { ssr: false }
+)
+
+const PrivyProvider = dynamic(
+  () => import('./privy').then(mod => mod.PrivyProvider),
+  { 
+    ssr: false,
+    loading: () => <div className="fixed inset-0 bg-background" />
+  }
+)
+//  修改结束
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode
 }
 
-const Providers: React.FC<Props> = ({ children }) => {
-    return (
-        <PostHogProvider>
-            <PrivyProvider>
-                <ColorModeProvider>
-                    <Analytics />
-                    {children}
-                </ColorModeProvider>
-            </PrivyProvider>
-        </PostHogProvider>
-    )
+export default function Providers({ children }: Props) {
+  return (
+    //  修改开始：使用动态加载的组件
+    <PostHogProvider>
+      <PrivyProvider>
+        <ColorModeProvider>
+          <Analytics />
+          {children}
+        </ColorModeProvider>
+      </PrivyProvider>
+    </PostHogProvider>
+    //  修改结束
+  )
 }
 
-export default Providers;
-
-export * from "./color-mode"
+export * from './color-mode'
